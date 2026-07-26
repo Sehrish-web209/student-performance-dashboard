20f96611c658da8539307105ab12ce86a8c02ca2
Sehrish <sehrmaqbool@gmail.com>
2026-07-25 20:11:55 -0700

    feat(Task 3b): implement findLast method to return last student with Grade "A"

--- src/StudentDashboard.jsx
+++ src/StudentDashboard.jsx
@@ -71,7 +71,7 @@ export default function StudentDashboard({students: studentList}) {
   // Task 3b: Find Last 'A' Student (findLast)
   const handleFindLastAStudent = () => {
     // TODO: Use students.findLast() to find the last student with Grade === "A"
-    const lastA = null; // Replace this line 
+    const lastA = students.findLast((student)=>student.Grade==="A");// Replace this line 
     if(!lastA){
         alert("Student not found!");
     }
